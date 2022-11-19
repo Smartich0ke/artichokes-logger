9ff4fd1188878f5d9818a1daf17bb90fad7a79c7
Smartich0ke <nikolaipatricknp@gmail.com>
2022-11-19 15:56:43 +1030

    create base class

---
+++ src/index.js
@@ -0,0 +1,1 @@
+exports.Logger = require('./logger');
---
+++ src/logger.js
@@ -0,0 +1,10 @@
+class Logger {
+    constructor() {
+        this.log("Logger is ready!");
+    }
+    log(message) {
+        console.log(message);
+    }
+}
+
+module.exports = Logger;
