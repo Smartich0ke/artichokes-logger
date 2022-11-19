cf9f414bb24951dd88d22aa488fb97473a462852
Smartich0ke <nikolaipatricknp@gmail.com>
2022-11-19 18:31:10 +1030

    create main log methods

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,43 @@
+const colour = require("node-console-colors");
 class Logger {
-    constructor() {
-        this.log("Logger is ready!");
+    infoPrefix = "[INFO] ";
+    warnPrefix = "[WARNING] ";
+    errorPrefix = "[ERROR] ";
+    interactionPrefix = "[INTERACTION]";
+
+    constructor() {    
+        this.info("Logger initialized!", true);
     }
     log(message) {
-        console.log(message);
+        console.log(colour.set("fg_default", message));
+    }
+    info(message, bold = false) {
+        if(bold) {
+        console.log(colour.set("fg_green", this.infoPrefix)+ colour.set("fg_gray", message));
+        } else {
+            console.log(colour.set("fg_green", this.infoPrefix) + colour.set("fg_default", message));
+        }
+    }
+    warn(message, bold = false) {
+        if(bold) {
+            console.log(colour.set("fg_yellow", this.warnPrefix) + colour.set("fg_gray", "bold", message));
+        } else {
+            console.log(colour.set("fg_yellow", this.warnPrefix) + colour.set("fg_default", message));
+        }
+    }
+    error(message, bold = false) {
+        if(bold) {
+            console.log(colour.set("fg_red", this.errorPrefix) + colour.set("fg_gray", "bold", message));
+        } else {
+            console.log(colour.set("fg_red", this.errorPrefix) + colour.set("fg_default", message));
+        }
+    }
+    interaction(message, interactionType = null, bold = false) {
+        if(bold) {
+            console.log(colour.set("fg_purple", this.interactionPrefix) + colour.set("fg_dark_purple", "(" + interactionType + ") ") + colour.set("fg_cyan", message));
+        } else {
+            console.log(colour.set("fg_purple", this.interactionPrefix) + colour.set("fg_dark_purple", "(" + interactionType + ") ") + colour.set("fg_dark_cyan", message));
+        }
     }
 }
 
